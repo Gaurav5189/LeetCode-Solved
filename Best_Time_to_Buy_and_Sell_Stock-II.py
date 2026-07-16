b0ebb1707645f07f956e23730ab36011b95d422f
Gaurav S. <gauravsethi4627@gmail.com>
2026-07-16 21:45:32 +0530

    solved 122. Best Time to Buy and Sell Stock II (medium)
    Input: prices = [7,1,5,3,6,4]
    Output: 7
    Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
    Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
    Total profit is 4 + 3 = 7

---
+++ Best_Time_to_Buy_and_Sell_Stock-II.py
@@ -0,0 +1,16 @@
+# 122. Best Time to Buy and Sell Stock II (medium)
+# Used Greedy method. Tc- O(n), Sc- O(1)
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += (prices[i] - prices[i-1])
+
+        return profit
+                
