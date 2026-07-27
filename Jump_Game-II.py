a5260ceef5cd151b2bd590d76bd1d7952b5ddb18
Gaurav S. <gauravsethi4627@gmail.com>
2026-07-28 02:58:03 +0530

    solved 45. Jump Game II
    Input: nums = [2,3,1,1,4]
    Output: 2
    Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.

---
+++ Jump_Game-II.py
@@ -0,0 +1,40 @@
+# 45. Jump Game II (medium)
+# Used recursion. Tc- O(n), Sc- O(n^2) or O(n) auxiliary space
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = [0]
+
+        def jump_count(combination):
+            l = len(combination)
+            goal = l-1
+
+            if l == 1:
+                return count[0]
+
+            for i in range(0, l-1):
+                if i + combination[i] >= goal:
+                    count[0] += 1
+                    jump_count(combination[0:i+1])
+                    break
+
+        jump_count(nums)
+        return count[0]
+
+# Alternative solution using greedy method
+# Tc- O(n), Sc- O(1)
+class Solution(object):
+    def jump(self, nums):
+        jumps = current_index = farthest = 0
+
+        for i in range(len(nums)-1):
+            farthest = max(farthest, i+nums[i])
+
+            if i == current_index:
+                jumps += 1
+                current_index = farthest
+
+        return jumps
